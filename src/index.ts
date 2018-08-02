f54766f25b3d9246873bc3d6c73c2c73eceb2ce5
itxuye <itxuye@gmail.com>
2018-08-02 09:48:41 +0800

    xxx

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import "reflect-metadata";
+import * as Koa from 'koa'
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { useKoaServer } from 'routing-controllers'
+import * as bodyParser from 'koa-bodyparser'
+import * as logger from 'koa-logger'
+import * as json from 'koa-json'
+import * as helmet from 'koa-helmet'
 
 createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-    
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-     
-    console.log("Here you can setup and run express/koa/any other framework.");
-    
+    const server = new Koa()
+    server.use(helmet())
+    server.use(json())
+    server.use(logger())
+    server.use(bodyParser())
+
+      // 绑定路由
+      const app = useKoaServer(server, {
+        controllers: [__dirname + '/controllers/*{.js,.ts,.tsx}']
+    })
+
+    app.listen(3001)
+    console.log(`Koa application is up and running on port ${3001}`)
 }).catch(error => console.log(error));
